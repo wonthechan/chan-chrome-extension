593807201f71048dbd8f1f501f221608d8cb42ff
wonthechan <packerstorm@gmail.com>
2019-11-17 20:46:59 +0900

    utilize localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,12 @@ if (localStorage.getItem('date')) {
 // 12시간 마다 한번씩 배경화면을 새로 불러온다.
 if (!storedDate || storedDate < date_today) {
   localStorage.removeItem('date');
-  localStorage.removeItem('wallpapers');
+  localStorage.removeItem('songs');
   date_today.setHours(date_today.getHours() + 12);
   localStorage.setItem('date', date_today);
 }
 
-let wallpapers = JSON.parse(localStorage.getItem('wallpapers'));
+let songs = JSON.parse(localStorage.getItem('songs'));
 
 function showBackgroundImage(path) {
   $('.body-wrapper').addClass('first-load');
@@ -20,71 +20,70 @@ function showBackgroundImage(path) {
     .attr('src', path)
     .on('load', function() {
       $(this).remove();
+
+
       $('.body-wrapper.first-load').css(
         'background-image',
         'url(' + path + ')'
       );
+      $('.body-wrapper.first-load').css(
+        'background-size',
+        'inherit'
+      );
+      $('.body-wrapper.first-load').css(
+        'background-repeat',
+        'no-repeat'
+      );
+
+
+  
+  // background: #eb01a5;
+  // background-image: url("IMAGE_URL"); /* fallback */
+  // background-image: url("IMAGE_URL"), linear-gradient(#eb01a5, #d13531); /* W3C */
+
       $('.background-overlay').addClass('loaded');
       $('.info-block').addClass('loaded');
     });
 }
 
 function getBgImage() {
-  let wallpapers = JSON.parse(localStorage.getItem('wallpapers'));
-  if (wallpapers) {
-    let randomWallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-    background_image = randomWallpaper.urls.regular
-    
+  let songs = JSON.parse(localStorage.getItem('songs'));
+  if (songs) {
+    let randomSong = songs[Math.floor(Math.random() * songs.length)];
+    background_image = randomSong.url
     console.log('getBgImage: ' + background_image)
     showBackgroundImage(background_image);
   }
 }
 
-function getNewWallpapers() {
-  var accessKey = '69fa36678b8cff77ff1c92f90d7e7ba7a42f412bbabc69e25e21940c474f742f'
-  let url = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=20&`;
-  let wallpapers = [];
-  $.getJSON(url, function(results) {
-    results.forEach(function(wallpaper) {
-      wallpapers.push(wallpaper);
-    });
-    localStorage.setItem('wallpapers', JSON.stringify(wallpapers));
-    getBgImage();
+function getNewSongs() {
+  let songs = [];
+  var url = 'https://cors-anywhere.herokuapp.com/www.melon.com/chart/index.htm';
+  $.get(url, function(response) {
+    listMusic = /<table[\s|\S]*?>[\s|\S]*?<\/table>/g.exec(response)[0].match(/<tr class="lst50"[\s|\S]*?>[\s|\S]*?<\/tr>/g);
+    for (i in listMusic) {
+      var data = new Object();
+      data.url = /src="([\S]*)\/melon/g.exec(listMusic[i])[1]
+      data.title = /<div class="ellipsis rank01"><span>[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
+      data.artist = /<div class="ellipsis rank02">[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
+      data.album = /<div class="ellipsis rank03">[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
+      // console.log(data.url);
+      // console.log(data.title);
+      // console.log(data.artist);
+      // console.log(data.album);
+      // console.log('\n')
+      songs.push(data);
+      localStorage.setItem('songs', JSON.stringify(songs));
+      getBgImage();
+    }
   });
 }
 
 
-
-
 $(document).ready(function() {
-  var name = "codemzy";
-  // https://cors-anywhere.herokuapp.com/
-  var url = 'https://cors-anywhere.herokuapp.com/www.melon.com/chart/index.htm';
-  console.log(url)
-
-  $.get(url, function(response) {
-    //console.log(response);
-    listMusic = /<table[\s|\S]*?>[\s|\S]*?<\/table>/g.exec(response)[0].match(/<tr class="lst50"[\s|\S]*?>[\s|\S]*?<\/tr>/g);
-
-    for (i in listMusic) {
-      //console.log(source)
-      urlAlbumArt = /src="([\S]*)\/melon/g.exec(listMusic[i])[1]
-      titleSong = /<div class="ellipsis rank01"><span>[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
-      artist = /<div class="ellipsis rank02">[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
-      titleAlbum = /<div class="ellipsis rank03">[\s|\S]*?>([\s|\S]*?)<\/a>/g.exec(listMusic[i])[1]
-      console.log(urlAlbumArt);
-      console.log(titleSong);
-      console.log(artist);
-      console.log(titleAlbum);
-      console.log('\n')
-    }
-
-  });
-
-
   getBgImage();
-  if (!wallpapers || wallpapers.length === 0) {
+  if (!songs || songs.length === 0) {
     // 배경화면 주소를 일괄적으로 20개 정도 가져옴.
-    getNewWallpapers();
+    getNewSongs();
   }
 });
